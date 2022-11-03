361199196a86bdcc63f9e1e80f7060ea97959b8a
gabriele.galizzi <gabriele.galizzi@gmail.com>
2022-11-03 16:07:06 +0100

    add DoneSurveyTableServletand ToDoSurveyTableServlet

---
+++ src/main/java/com/example/survey/ToDoSurveyTableServlet.java
@@ -0,0 +1,39 @@
+package com.example.survey;
+
+import com.google.gson.Gson;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@WebServlet(name = "ToDoSurveyTableServlet", value = "/ToDoSurveyTableServlet")
+public class ToDoSurveyTableServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int step = Integer.parseInt(request.getParameter("step"));
+        int currentPage = Integer.parseInt(request.getParameter("currentpage"));
+        String username = getInitParameter("username");
+        Read r = new Read();
+        ArrayList<Survey> arrSurvey = null;
+        try {
+            arrSurvey = r.readSurveyToDo("root","123456",currentPage,step,username);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+        Gson gson = new Gson();
+        String json = gson.toJson(arrSurvey);
+
+        PrintWriter out = response.getWriter();
+        out.print(json);
+
+    }
+}
